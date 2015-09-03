ef734e84f8fd7bec1240778702be556c191a17b3
Maxime Charron <maxime.charron.4@ulaval.ca>
2015-09-03 17:09:31 -0400

    Remove jquery because no frameworks authorized and remove unused css

--- Labo1/js/randomProverb.js
+++ Labo1/js/randomProverb.js
@@ -1,15 +1,30 @@
 /**
  * Created by macha762 on 2015-09-03.
  */
-$(document).ready(
-    function(){
-        $('.randomProverb').each(function(index, elem) {
-            $.ajax({
-                url: "https://api.github.com/zen",
-                cache: false,
-                success: function(html){
-                    $(elem).html(html);
-                }
-            });
-        })
-        });
+
+var loadXMLDoc = function () {
+    var xmlhttp;
+
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) {
+                document.getElementsByClassName("randomProverb").forEach(function (elem) {
+                    elem.innerHTML = xmlhttp.responseText;
+                })
+            }
+        }
+
+        xmlhttp.open("GET", "https://api.github.com/zen", true);
+        xmlhttp.send();
+    }
+};
+
+window.onload = loadXMLDoc();
