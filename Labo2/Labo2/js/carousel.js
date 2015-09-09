83c1e150bdb66e03f40fd920b55230d7bfa0c111
Antoine Lefrançois <antoine.lefrancois.1@ulaval.ca>
2015-09-09 15:02:57 -0400

    Refactored the carousel file

--- Labo2/Labo2/js/carousel.js
+++ Labo2/Labo2/js/carousel.js
@@ -1,90 +1,86 @@
-slidePrefix = "slide-";
-slideControlPrefix = "slide-control-";
-slideHighlightClass = "highlight";
-slidesContainerID = "slides";
-slidesControlsID = "slides-controls";
-slideDelay = 3000;
-slideAnimationInterval = 20;
-slideTransitionSteps = 10;
+var slidePrefix = "slide-";
+var slideControlPrefix = "slide-control-";
+var slideHighlightClass = "highlight";
+var slidesContainerID = "slides";
+var slidesControlsID = "slides-controls";
+var slideDelay = 3000;
+var slideAnimationInterval = 20;
+var slideTransitionSteps = 10;
+var slideTransStep = 0;
+var transTimeout = 0;
+var crtSlideIndex = 1;
+var slidesCollection = [];
+var slidesPages = [];
+var nextSlideIndex = 0;
+var previousSlideIndex = 0;
+var numberOfSlides = 0;
+var nextSlide;
+var currentSlide;
 
 function setUpSlideShow() {
-    // collect the slides and the controls
+
     slidesCollection = document.getElementById(slidesContainerID).children;
-    slidesControllersCollection = document.getElementById(slidesControlsID).children;
+    slidesPages = document.getElementById(slidesControlsID).children;
 
-    totalSlides = slidesCollection.length;
+     numberOfSlides = slidesCollection.length;
+    for (var i = 0; i < numberOfSlides; i++) {
 
-    //go through all slides
-    for (var i = 0; i < slidesCollection.length; i++) {
-        // give IDs to slides and controls
-        slidesCollection[i].id = slidePrefix + (i + 1);
-        slidesControllersCollection[i].id = slideControlPrefix + (i + 1);
+        slidesCollection[i].id = slidePrefix + (1 + i);
+        slidesPages[i].id = slideControlPrefix + (i + 1);
 
-        // attach onclick handlers to controls, highlight the first control
-        slidesControllersCollection[i].onclick = function () {
-            clickSlide(this);
+        slidesPages[i].onclick = function () {
+            changeSlideByPage(this);
         };
-
-        //hide all slides except the first
         if (i > 0) {
             slidesCollection[i].style.display = "none";
         } else {
-            slidesControllersCollection[i].className = slideHighlightClass;
+            slidesPages[i].className = slideHighlightClass;
+
         }
+        document.onkeydown = detectChar;
     }
-
-    // initialize vars
-    slideTransStep = 0;
-    transTimeout = 0;
-    crtSlideIndex = 1;
-
-    document.onkeydown = detectChar;
-
-    // show the next slide
-    showSlide(2);
 }
+
 
 function detectChar(event){
     var charCode = event.keyCode;
     if (charCode == 39) {
-        showSlide(nextSlideIndex, true);
+        switchSlide(nextSlideIndex, true);
     }
     if (charCode == 37) {
-        showSlide(previousSlideIndex, true);
+        switchSlide(previousSlideIndex, true);
     }
 }
 
-function showSlide(slideNo, immediate) {
-    // don't do any action while a transition is in progress
+function switchSlide(slideNo, immediate) {
     if (slideTransStep != 0 || slideNo == crtSlideIndex)
         return;
 
     clearTimeout(transTimeout);
 
-    // get references to the current slide and to the one to be shown next
     if (slideNo <= 0) {
-        slideNo = totalSlides;
+        slideNo = numberOfSlides;
     }
     previousSlideIndex = crtSlideIndex - 1;
     nextSlideIndex = slideNo;
-    crtSlide = document.getElementById(slidePrefix + crtSlideIndex);
+    currentSlide = document.getElementById(slidePrefix + crtSlideIndex);
     nextSlide = document.getElementById(slidePrefix + nextSlideIndex);
     slideTransStep = 0;
 
     // start the transition now upon request or after a delay (default)
     if (immediate == true) {
-        transSlide();
+        slideTransition();
     } else {
-        transTimeout = setTimeout("transSlide()", slideDelay);
+        transTimeout = setTimeout("slideTransition()", slideDelay);
     }
 }
 
-function clickSlide(control) {
-    showSlide(Number(control.id.substr(control.id.lastIndexOf("-") + 1)), true);
+function changeSlideByPage(control) {
+    switchSlide(Number(control.id.substr(control.id.lastIndexOf("-") + 1)), true);
 }
 
 
-function transSlide() {
+function slideTransition() {
     // make sure the next slide is visible (albeit transparent)
     nextSlide.style.display = "block";
 
@@ -93,8 +89,8 @@ function transSlide() {
     console.log(opacity);
 
     // fade out the current slide
-    crtSlide.style.opacity = "" + (1 - opacity);
-    crtSlide.style.filter = "alpha(opacity=" + (100 - opacity * 100) + ")";
+    currentSlide.style.opacity = "" + (1 - opacity);
+    currentSlide.style.filter = "alpha(opacity=" + (100 - opacity * 100) + ")";
 
     // fade in the next slide
     nextSlide.style.opacity = "" + opacity;
@@ -102,26 +98,26 @@ function transSlide() {
 
     // if not completed, do this step again after a short delay
     if (++slideTransStep <= slideTransitionSteps) {
-        transTimeout = setTimeout("transSlide()", slideAnimationInterval);
+        transTimeout = setTimeout("slideTransition()", slideAnimationInterval);
     } else {
-        // complete
-        crtSlide.style.display = "none";
-        transComplete();
+        currentSlide.style.display = "none";
+        transitionCompleted();
+        clearTimeout(transTimeout);
     }
 }
 
-function transComplete() {
+function transitionCompleted() {
     slideTransStep = 0;
     crtSlideIndex = nextSlideIndex;
 
-    // show next slide
-    showSlide((crtSlideIndex >= totalSlides) ? 1 : crtSlideIndex + 1);
+    switchSlide((crtSlideIndex >= numberOfSlides) ? 1 : crtSlideIndex + 1);
 
     //unhighlight all controls
-    for (var i = 0; i < slidesControllersCollection.length; i++) {
-        slidesControllersCollection[i].className = "";
+    for (var i = 0; i < slidesPages.length; i++) {
+        slidesPages[i].className = "";
     }
 
     // highlight the control for the next slide
-    document.getElementById("slide-control-" + crtSlideIndex).className = slideHighlightClass;
+    slidesPages[crtSlideIndex - 1].className = slideHighlightClass;
+
 }
