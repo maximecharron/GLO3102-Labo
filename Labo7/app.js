c4c091d947f0754d5e3cc41d97ee11d4eaf88aa8
Maxime Charron <charron.maxime97@gmail.com>
2015-11-02 18:44:46 -0500

    Added geolocalisation for labo7

--- Labo7/app.js
+++ Labo7/app.js
@@ -1,11 +1,27 @@
 $(function () {
-    var weeklyForecast = new WeeklyForecastCollection({});
-    weeklyForecast.url = 'http://api.wunderground.com/api/893bb702be7668e4/forecast10day/q/canada/levis.json';
+    if (navigator.geolocation) {
+        // Get the current position.
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var weeklyForecast = new WeeklyForecastCollection({});
+            query = position.coords.latitude + ',' + position.coords.longitude;
+            weeklyForecast.url = 'http://api.wunderground.com/api/893bb702be7668e4//forecast10day/geolookup/q/' + query + '.json';
+            var view = new ForecastView({
+                collection: weeklyForecast
+            });
+            weeklyForecast.fetch({
+                dataType: 'jsonp'
+            });
 
-    var view = new ForecastView({
-        collection: weeklyForecast
-    });
-    weeklyForecast.fetch({
-        dataType: 'jsonp'
-    });
+        });
+    } else {
+        var weeklyForecast = new WeeklyForecastCollection({});
+        weeklyForecast.url = 'http://api.wunderground.com/api/893bb702be7668e4/forecast10day/q/canada/montreal.json';
+
+        var view = new ForecastView({
+            collection: weeklyForecast
+        });
+        weeklyForecast.fetch({
+            dataType: 'jsonp'
+        });
+    }
 });
