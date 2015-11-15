e4181e22057cfb452a975799c58948ac9e7b8643
Maxime Charron <charron.maxime97@gmail.com>
2015-11-15 14:09:00 -0500

    Finished labo8

--- Labo8/tasks.js
+++ Labo8/tasks.js
@@ -1,10 +1,10 @@
 /**
  * Created by macha762 on 2015-11-09.
  */
-var tasks = [];
+var tasksList = [];
 
 exports.getAll = function (req, res) {
-    res.json({ tasks: tasks });
+    res.json({tasks: tasks});
 };
 
 exports.create = function (req, res) {
@@ -14,46 +14,49 @@ exports.create = function (req, res) {
             message: 'You must pass a task in the body'
         });
     }
-    tasks.push(req.body);
+    tasksList.push(req.body);
     res.send(201);
 };
 
-exports.update= function (req, res) {
+exports.update = function (req, res) {
     if (req.body.task === undefined) {
         res.status(400).send({
             error: 'BODY_TASK_EMPTY',
             message: 'You must pass a task in the body'
         });
     }
+
     var updateIndex = -1;
-    tasks.forEach(function(task) {
+    tasksList.forEach(function (task) {
         if (task.id == req.params.id) {
-            updateIndex = task.indexOf(task);
+            updateIndex = tasksList.indexOf(task);
         }
-    });
+    })
+    ;
     if (updateIndex !== -1) {
-        tasks.splice(updateIndex, 1);
-        tasks.push(req.body);
-        res.send(200);
+        tasksList.splice(updateIndex, 1);
+        tasksList.push(req.body);
+        res.sendStatus(200);
     } else {
         res.status(404).send({
             error: 'TASK_NOT_FOUND',
             message: 'Given task was not found.'
         });
     }
-};
+}
+;
 
 exports.delete = function (req, res) {
     var deleteIndex = -1;
 
-    tasks.forEach(function(task, index) {
+    tasksList.forEach(function (task) {
         if (task.id == req.params.id) {
-            deleteIndex = index;
+            deleteIndex = tasksList.indexOf(task);
         }
     });
 
     if (deleteIndex !== -1) {
-        tasks.splice(deleteIndex, 1);
+        tasksList.splice(deleteIndex, 1); //Remove 1 element at index deleteIndex
         res.send(200);
     } else {
         res.status(404).send({
