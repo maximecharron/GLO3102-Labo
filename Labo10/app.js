8259e6c64252734b6dd0d506f2fb6ebef7ada644
Antoine Lefrancois <antoine.lefrancois.1@ulaval.ca>
2015-11-29 10:19:51 -0500

    Adding messages history, and sending it to others on connection

--- Labo10/app.js
+++ Labo10/app.js
@@ -4,7 +4,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var messages = [];
+
 app.get('/', function(req, res){
     res.sendfile('index.html');
 });
@@ -17,16 +17,20 @@ app.get('/js/client.js', function(req, res){
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
+
+var messages = [];
+
 io.on('connection', function(socket){
     console.log('a user connected');
-    socket.send(messages);
+    messages.forEach(function(message){
+        socket.emit('chat message', message);
+    })
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
     socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
         io.emit('chat message', msg);
-        messages.add(msg);
+        messages.push(msg);
     });
 
 });
--- Labo10/js/client.js
+++ Labo10/js/client.js
@@ -2,8 +2,12 @@
  * Created by Antoine on 11/28/2015.
  */
 var socket = io();
+var person = "";
+while(person.length == 0){
+    var person = prompt("Please enter your name");
+}
 $('form').submit(function(){
-    socket.emit('chat message', $('#message').val());
+    socket.emit('chat message', person +" - " +$('#message').val());
     $('#message').val('');
     return false;
 });
